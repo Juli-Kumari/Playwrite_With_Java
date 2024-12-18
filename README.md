1fdb47899e3a4e5fac94b5753f049b75f1fa6c83
juli <julikumariece76@gmail.com>
2024-12-18 15:35:51 +0530

    commit 2

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # PlaywrightWithJava
+
+Pre Install:- Java (java --version), Maven (mvn --version)
+
 URL  = https://playwright.dev/java/
 1. Playwright supports all modern rendering engines including Chromium, WebKit, and Firefox.
 2. Test on Windows, Linux, and macOS, locally or on CI, headless or headed with native mobile emulation.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import com.microsoft.playwright.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+        }
     }
 }
