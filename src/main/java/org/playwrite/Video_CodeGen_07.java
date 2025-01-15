46646a8b9a5e9bb2550bae61ef113dd568d9cbae
juli <julikumariece76@gmail.com>
2025-01-15 21:20:50 +0530

    add files

--- src/main/java/org/playwrite/Video_CodeGen_07.java
+++ src/main/java/org/playwrite/Video_CodeGen_07.java
@@ -1,4 +1,26 @@
 package org.playwrite;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 public class Video_CodeGen_07 {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("https://the-internet.herokuapp.com/");
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Challenging DOM")).click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("qux")).click();
+            page.getByRole(AriaRole.ROW, new Page.GetByRoleOptions().setName("Iuvaret5 Apeirian5 Adipisci5")).getByRole(AriaRole.LINK).nth(1).click();
+            page.getByRole(AriaRole.ROW, new Page.GetByRoleOptions().setName("Iuvaret5 Apeirian5 Adipisci5")).getByRole(AriaRole.LINK).first().click();
+            page.locator("#canvas").click(new Locator.ClickOptions()
+                    .setPosition(216, 96));
+//            assertThat(page.locator("#canvas")).isVisible();
+page.close();
+browser.close();
+        }
+    }
 }
